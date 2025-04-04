9b6a78f03b658ad108641f36774cacfb2480a530
Jeckhat <jeckhat@gmail.com>
2025-04-04 17:41:36 +0700

    feat: stake model

---
+++ src/models/Stake.ts
@@ -0,0 +1,74 @@
+import { PublicKey } from "@solana/web3.js"
+import dayjs from "dayjs";
+import { Numeric } from "./Numeric";
+
+export class Stake {
+    authority?: string
+    balance?: number
+    boost?: string
+    lastClaimAt?: dayjs.Dayjs
+    lastDespositAt?: dayjs.Dayjs
+    lastWithdrawAt?: dayjs.Dayjs // .format('YYYY-MM-DD HH:mm:ss')
+    lastRewardsFactor?: Numeric
+    rewards?: number
+
+    constructor(authority?: string, balance?: number, boost?: string, lastClaimAt?: dayjs.Dayjs, lastDespositAt?: dayjs.Dayjs,
+        lastWithdrawAt?: dayjs.Dayjs, lastRewardsFactor?: Numeric, rewards?: number) {
+        this.authority = authority;
+        this.balance = balance;
+        this.boost = boost;
+        this.lastClaimAt = lastClaimAt;
+        this.lastDespositAt = lastDespositAt;
+        this.lastWithdrawAt = lastWithdrawAt;
+        this.lastRewardsFactor = lastRewardsFactor;
+        this.rewards = rewards;
+    }
+}
+
+function tryFromBytes(buffer: Buffer) {
+    try {
+        let offset = 0;
+        offset += 8;
+
+        const authority = new PublicKey(buffer.subarray(offset, offset + 32)).toString();
+        offset += 32;
+
+        const balance = Number(buffer.readBigUInt64LE(offset));
+        offset += 8;
+
+        const boost = new PublicKey(buffer.subarray(offset, offset + 32)).toString();
+        offset += 32;
+
+        const lastClaimAt = dayjs.unix(Number(buffer.readBigInt64LE(offset)));
+        offset += 8;
+
+        const lastDespositAt = dayjs.unix(Number(buffer.readBigInt64LE(offset)));
+        offset += 8;
+
+        const lastWithdrawAt = dayjs.unix(Number(buffer.readBigInt64LE(offset)));
+        offset += 8;
+
+        const lastRewardsFactor = new Numeric(Array.from(buffer.slice(offset, offset + 16)));
+        offset += 16;
+
+        const rewards = Number(buffer.readBigUInt64LE(offset));
+        offset += 8;
+
+        return new Stake(authority, balance, boost, lastClaimAt, lastDespositAt, lastWithdrawAt, lastRewardsFactor, rewards);
+    } catch (error) {
+        return null;
+    }
+}
+
+export async function getStakeResult(data?: Buffer) {
+    if (!data) {
+        throw new Error("Failed to get miner account");
+    }
+
+    const proof = tryFromBytes(data);
+    if (!proof) {
+        throw new Error("Failed to parse miner account");
+    }
+
+    return proof;
+}
--- src/models/index.ts
+++ src/models/index.ts
@@ -1,3 +1,4 @@
 export { CustomError } from './Error'
 export { Proof, getProofResult } from './Proof'
 export { Boost, getBoostResult } from './Boost'
+export { Stake, getStakeResult } from './Stake'
