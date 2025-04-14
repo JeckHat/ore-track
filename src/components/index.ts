9e350491785b3d1fca8d3b3779931bb8a5f1402e
Jeckhat <jeckhat@gmail.com>
2025-04-14 18:24:22 +0700

    feat: import modaloptions

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,5 @@
 export { CustomText } from './Texts'
 export { CheckBox, Input } from './Forms'
 export { Button, ButtonIcon } from './Buttons'
-export { LoadingModal, BottomModal, ModalTransaction } from './Modals'
+export { LoadingModal, BottomModal, ModalTransaction, ModalImportOptions } from './Modals'
 export { KeyboardDismissPressable, SkeletonLoader, OptionMenu } from './Views'
