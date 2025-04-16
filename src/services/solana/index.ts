493b94d9c0b36ab6a3a132e474d0c2c91fe6c527
Jeckhat <jeckhat@gmail.com>
2025-04-17 05:38:49 +0700

    fix: helius get priority fee

--- src/services/solana/index.ts
+++ src/services/solana/index.ts
@@ -1,4 +1,4 @@
-import { LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js"
+import { LAMPORTS_PER_SOL, PublicKey, Transaction, VersionedTransaction } from "@solana/web3.js"
 
 import { CustomError } from "@models"
 import { SOL_MINT } from "@constants"
@@ -32,4 +32,58 @@ export async function getTokenBalance(tokenAddress: string): Promise<number> {
     const connection = getConnection()
     const tokenAmount = await connection.getTokenAccountBalance(new PublicKey(tokenAddress))
     return tokenAmount?.value?.uiAmount ?? 0
+}
+
+export async function getPriorityFee(transaction: Transaction | VersionedTransaction) {
+    try {
+        const connection = getConnection()
+
+        let publicKeys: string[] = []
+        if ('version' in transaction) {
+            publicKeys = transaction.message.getAccountKeys().staticAccountKeys.map(key => key.toBase58())
+        } else {
+            publicKeys = transaction.compileMessage().accountKeys.map(key => key.toBase58())
+        }
+
+        const response = await fetch(connection.rpcEndpoint, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                jsonrpc: '2.0',
+                id: 'helius-priority-fee',
+                method: 'getPriorityFeeEstimate',
+                params: [
+                    {
+                        accountKeys: publicKeys,
+                        options: {
+                            recommended: true,
+                        },
+                    }
+                ],
+            }),
+        });
+    
+        const data = await response.json();
+        return data.result?.priorityFeeEstimate;
+        // if (rpcProvider === 'quicknode') {
+        //     console.log("quicknode msuk")
+        //     const response = await fetch(connection.rpcEndpoint, {
+        //         method: 'POST',
+        //         headers: { 'Content-Type': 'application/json' },
+        //         body: JSON.stringify({
+        //             jsonrpc: '2.0',
+        //             id: 'quicknode-priority-fee',
+        //             method: 'qn_estimatePriorityFees',
+        //             params: { "last_n_blocks": 100, "api_version": 2, "account": PROGRAM_ID }
+        //         }),
+        //     });
+
+        //     const data = await response.json()
+        //     console.log("data", data)
+        //     return 100
+        // }
+    } catch(error) {
+        console.log("error", error)
+        return 10000
+    }
 }
